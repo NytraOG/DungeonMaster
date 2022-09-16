c0e502d4d77557c5e85f79ea2e00837a4bb1377d
Elm <elm.faiad@gmail.com>
2022-09-16 20:40:11 +0200

    OLOLOl Feuerball BUTZ

---
+++ Assets/Skills/Feuerball.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feuerball : BaseSkill
+{
+    public override skillCategory Category => skillCategory.Magic;
+
+
+    public void Start()
+    {
+        Name = "Feuerball";    
+            
+            }
+}
