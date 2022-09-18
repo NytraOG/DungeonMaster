34c25d43d6416a88d39497270351192987f69949
Elm <elm.faiad@gmail.com>
2022-09-18 12:39:45 +0200

    Terst2

--- Assets/Entities/Classes/Assassin.cs
+++ Assets/Entities/Classes/Assassin.cs
@@ -4,17 +4,17 @@
     {
         public Assassin()
         {
-            Strength           = 1;
-            Constitution     = 1;
-            Dexterity        = 1;
-            Quickness       = 1;
-            Intuition        = 2;
-            Logic            = 1;
-            Willpower     = 1;
-            Wisdom         = 1;
-            Charisma         = 1;
-            Hitpoints     = HitpointsMax;
-            Schaden          = 1;
+            Strength            = 1;
+            Constitution        = 1;
+            Dexterity           = 1;
+            Quickness           = 1;
+            Intuition           = 2;
+            Logic               = 1;
+            Willpower           = 1;
+            Wisdom              = 1;
+            Charisma            = 1;
+            Hitpoints           = HitpointsMax;
+            Schaden             = 1;
 
             InitiativeBestimmen();
         }
@@ -24,7 +24,7 @@
         public override int DealDamage(BaseUnit target)
         {
             var damageDealt = Schaden * Schadensmodifier;
-            target.Lebenspunkte -= damageDealt;
+            target.Hitpoints -= damageDealt;
 
             return (int)damageDealt;
         }
