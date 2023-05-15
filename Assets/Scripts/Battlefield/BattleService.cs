4b59eec05204f30947772035e138b75524c359bb
Christian Nytra <christian.nytra@co-orga.d>
2023-05-15 02:53:01 +0200

    letsgo

--- Assets/Scripts/Battlefield/BattleService.cs
+++ Assets/Scripts/Battlefield/BattleService.cs
@@ -12,18 +12,20 @@ namespace Battlefield
 {
     public class BattleService : MonoBehaviour
     {
-        public  GameObject        healthbarPrefab;
-        public  List<Creature>    enemies;
-        public  List<GameObject>  heroes;
-        public  List<GameObject>  enemiesToSpawn = new();
-        public  InventoryItemData itemData1;
-        public  InventoryItemData itemData2;
-        public  InventoryItemData itemData3;
-        public  InventoryItemData itemData4;
-        public  GameObject        inventoryPanel;
-        public  GameObject        openBagAudioSource;
-        private List<BaseUnit>    combatants = new();
-        private bool              inventoryShown;
+        public  GameObject                healthbarPrefab;
+        public  List<Creature>            enemies;
+        public  List<GameObject>          heroes;
+        public  List<GameObject>          enemiesToSpawn = new();
+        public  InventoryItemData         itemData1;
+        public  InventoryItemData         itemData2;
+        public  InventoryItemData         itemData3;
+        public  InventoryItemData         itemData4;
+        public  GameObject                inventoryPanel;
+        public  GameObject                openBagAudioSource;
+        private List<BaseUnit>            combatants = new();
+        private Vector3[]                 enemyTransforms;
+        private Dictionary<Vector3, bool> fieldslots;
+        private bool                      inventoryShown;
 
         public void Start() => InitScene();
 
@@ -91,42 +93,87 @@ namespace Battlefield
             controller.allesDa          = false;
         }
 
+        public void SpawnEnemy()
+        {
+            var keyToUpdate = new Vector3();
+
+            foreach (var fieldslot in fieldslots)
+            {
+                if (fieldslot.Value)
+                    continue;
+
+                var index = new Random().Next(0, enemiesToSpawn.Count);
+
+                CreateCreature(index, fieldslot.Key);
+
+                keyToUpdate = fieldslot.Key;
+
+                break;
+            }
+
+            fieldslots[keyToUpdate] = true;
+        }
+
         private void InitScene()
         {
             inventoryShown = inventoryPanel.GetComponent<StaticInventoryDisplay>().showInventory;
 
-            var enemyTransforms = new[]
+            enemyTransforms = new[]
             {
                 new Vector3(1.78f, 0.9f),
-                new Vector3(2.86f, 1.5f)
+                new Vector3(2.86f, 1.5f),
+                new Vector3(-0.16f, -0.21f),
+                new Vector3(-1.31f, -0.83f),
+                new Vector3(4.58f, 2.44f),
+                new Vector3(5.56f, 2.89f),
+                new Vector3(1.45f, 2.12f)
+            };
+
+            fieldslots = new Dictionary<Vector3, bool>
+            {
+                { enemyTransforms[0], false },
+                { enemyTransforms[1], false },
+                { enemyTransforms[2], false },
+                { enemyTransforms[3], false },
+                { enemyTransforms[4], false },
+                { enemyTransforms[5], false },
+                { enemyTransforms[6], false }
             };
 
             for (var i = 0; i < enemiesToSpawn.Count; i++)
             {
-                var enemyGameobject = Instantiate(enemiesToSpawn[i], enemyTransforms[i], Quaternion.identity);
-                var enemy           = enemyGameobject.GetComponent<Creature>();
-                var healthbar       = Instantiate(healthbarPrefab, enemy.transform);
+                CreateCreature(i, enemyTransforms[i]);
 
-                healthbar.GetComponent<HealthpointBar>().unit = enemy;
-
-                var canvas = healthbar.transform.Find("Canvas")
-                                      .gameObject.GetComponent<Canvas>();
-
-                var background    = canvas.transform.Find("Border");
-                var missinghealth = canvas.transform.Find("MissingHealth");
-                var currenthealth = canvas.transform.Find("CurrentHealth");
-
-                var enemyposition = enemy.gameObject.transform.position;
-
-                var enemyWidth  = enemy.GetComponent<RectTransform>().rect.width;
-                var enemyHeight = enemy.GetComponent<RectTransform>().rect.height;
-
-                background.transform.position    = new Vector3(enemyposition.x * 100 + 960 + enemyWidth / 2, enemyposition.y * 100 + 540 + enemyHeight, enemyposition.z);
-                missinghealth.transform.position = new Vector3(enemyposition.x * 100 + 960 + enemyWidth / 2, enemyposition.y * 100 + 540 + enemyHeight, enemyposition.z);
-                currenthealth.transform.position = new Vector3(enemyposition.x * 100 + 960 + enemyWidth / 2, enemyposition.y * 100 + 540 + enemyHeight, enemyposition.z);
-
-                enemies.Add(enemy);
+                fieldslots[enemyTransforms[i]] = true;
             }
+        }
+
+        private void CreateCreature(int enemyIndex, Vector3 enemyTransform)
+        {
+            var enemyGameobject = Instantiate(enemiesToSpawn[enemyIndex], enemyTransform, Quaternion.identity);
+            var enemy           = enemyGameobject.GetComponent<Creature>();
+            var healthbar       = Instantiate(healthbarPrefab, enemy.transform);
+
+
+            healthbar.GetComponent<HealthpointBar>().unit = enemy;
+
+            var canvas = healthbar.transform.Find("Canvas")
+                                  .gameObject.GetComponent<Canvas>();
+
+            var background    = canvas.transform.Find("Border");
+            var missinghealth = canvas.transform.Find("MissingHealth");
+            var currenthealth = canvas.transform.Find("CurrentHealth");
+
+            var enemyposition = enemy.gameObject.transform.position;
+
+            var enemyWidth  = enemy.GetComponent<RectTransform>().rect.width;
+            var enemyHeight = enemy.GetComponent<RectTransform>().rect.height;
+
+            background.transform.position    = new Vector3(enemyposition.x * 100 + 960 + enemyWidth / 2, enemyposition.y * 100 + 540 + enemyHeight, enemyposition.z);
+            missinghealth.transform.position = new Vector3(enemyposition.x * 100 + 960 + enemyWidth / 2, enemyposition.y * 100 + 540 + enemyHeight, enemyposition.z);
+            currenthealth.transform.position = new Vector3(enemyposition.x * 100 + 960 + enemyWidth / 2, enemyposition.y * 100 + 540 + enemyHeight, enemyposition.z);
+
+            enemies.Add(enemy);
         }
 
         private void PlayOpenBagSound()
