bc48796ebffc052fdd98ccda4221fde7ae011383
Christian Nytra <christian.nytra@co-orga.d>
2023-05-21 03:05:11 +0200

    ok

--- Assets/Scripts/UI/StatusPanel.cs
+++ Assets/Scripts/UI/StatusPanel.cs
@@ -1,6 +1,7 @@
 using Entities.Hero;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI
 {
@@ -15,11 +16,30 @@ namespace UI
         public  TextMeshProUGUI textWillpower;
         public  TextMeshProUGUI textWisdom;
         public  TextMeshProUGUI textName;
+        public  TextMeshProUGUI textMeleeDefense;
+        public  TextMeshProUGUI textRangedDefense;
+        public  TextMeshProUGUI textMagicDefense;
+        public  TextMeshProUGUI textSocialDefense;
+        public  TextMeshProUGUI textInitiativeBase;
+        public  TextMeshProUGUI textHealthOrbPercentage;
+        public  TextMeshProUGUI textManaOrbPercentage;
+        public  Image           currentHealth;
+        public  Image           currenMana;
         private bool            assignmentChanged;
+        private float           healthSnapshot;
+        private float           manaSnapshot;
         private Hero            selectedHero;
+
+        private void Awake()
+        {
+            textHealthOrbPercentage.gameObject.SetActive(false);
+            textManaOrbPercentage.gameObject.SetActive(false);
+        }
 
         private void Update()
         {
+            UpdateOrbs();
+
             if (!assignmentChanged)
                 return;
 
@@ -33,7 +53,42 @@ namespace UI
             textWillpower.text    = selectedHero.Willpower.ToString();
             textWisdom.text       = selectedHero.Wisdom.ToString();
 
+            textMeleeDefense.text   = selectedHero.ModifiedMeleeDefense.ToString("####");
+            textRangedDefense.text  = selectedHero.ModifiedRangedDefense.ToString("####");
+            textMagicDefense.text   = selectedHero.ModifiedMagicDefense.ToString("####");
+            textSocialDefense.text  = selectedHero.ModifiedSocialDefense.ToString("####");
+            textInitiativeBase.text = selectedHero.BaseInitiative.ToString("####");
+
             assignmentChanged = false;
+        }
+
+        public void ShowPercentagesHealth() => textHealthOrbPercentage.gameObject.SetActive(true);
+
+        public void ShowPercentagesMana() => textManaOrbPercentage.gameObject.SetActive(true);
+
+        public void HidePercentagesHealth() => textHealthOrbPercentage.gameObject.SetActive(false);
+
+        public void HidePercentagesMana() => textManaOrbPercentage.gameObject.SetActive(false);
+
+        private void UpdateOrbs()
+        {
+            var healthUpdated = (int)selectedHero.CurrentHitpoints != (int)healthSnapshot;
+
+            if (healthUpdated)
+            {
+                textHealthOrbPercentage.text = $"{(int)(selectedHero.CurrentHitpoints / selectedHero.MaximumHitpoints * 100)}%";
+                currentHealth.fillAmount     = selectedHero.CurrentHitpoints / selectedHero.MaximumHitpoints;
+                healthSnapshot               = selectedHero.CurrentHitpoints;
+            }
+
+            var manaUpdated = (int)selectedHero.CurrentMana != (int)manaSnapshot;
+
+            if (manaUpdated)
+            {
+                textManaOrbPercentage.text = $"{(int)(selectedHero.CurrentMana / selectedHero.MaximumMana * 100)}%";
+                currenMana.fillAmount      = selectedHero.CurrentMana / selectedHero.MaximumMana;
+                manaSnapshot               = selectedHero.CurrentMana;
+            }
         }
 
         public void ChangeHero(Hero hero)
